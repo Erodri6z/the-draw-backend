28b52a8a6f7b9eb6aaa0067ac62ea49bc2b89c3d
Erodri6z <erikrodriguez2417@gmail.com>
2025-06-06 00:23:03 -0500

    add a controller function that should help render images

---
+++ controllers/tarot.js
@@ -0,0 +1,23 @@
+import fetch from 'node-fetch'
+
+async function proxyImage(req, res){
+  const { slug } = req.params
+
+  const cloudName = process.env.CLOUDINARY_CLOUD_NAME
+  const imageUrl = `https://res.cloudinary.com/${cloudName}/image/upload/tarot/${slug}.jpg`
+
+  try {
+    const response = await fetch(imageUrl);
+    const contentType = response.headers.get('content-type')
+    const buffer = await response.arrayBuffer()
+
+    res.set('Content-Type', contentType)
+    res.send(Buffer.from(buffer))
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to load image', message: err.message })
+  }
+}
+
+export {
+  proxyImage
+}
