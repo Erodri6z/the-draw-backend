fd220740d600cae1277d9b6947407727e28788d7
Erodri6z <erikrodriguez2417@gmail.com>
2025-07-07 20:47:14 -0500

    add some more params for the prompt

--- controllers/openai.js
+++ controllers/openai.js
@@ -17,12 +17,13 @@ async function generateResponse(req, res){
 
     const completion = await openai.chat.completions.create({
       model: 'gpt-4o-mini',
-      messages: [{ role: 'user', content: prompt + "Please anwser in 3 paragraphs max" }],
+      messages: [{ role: 'user', content: prompt + "Please anwser in 9 Sentences Max and under 300 tokens with the card meanings in mind regardless of if its positive or not. Also Include a final sentence with advice" }],
       max_tokens : 300
     })
 
     const message = completion.choices[0].message.content
     res.json({ response: message.replace(/\n/g, " ") })
+    console.log({ response: message.replace(/\n/g, " ") })
   } catch (error) {
     console.error("Somethings wrong with openai", error)
     res.status(500).json({ error: 'Failed to generate response'})
